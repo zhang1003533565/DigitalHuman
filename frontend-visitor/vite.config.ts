e00942d72ada51d1333138313cfef5ecdbb165b4
Nght owl <liu201885524@qq.com>
2026-06-15 17:29:42 +0800

    更改端口

--- frontend-visitor/vite.config.ts
+++ frontend-visitor/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 30001,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
